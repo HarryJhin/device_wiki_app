5f0beed15e9c64d7cb55d0e77327b0db2e7dbd79
JJH <jujinhyen@gmail.com>
2021-12-01 09:53:00 +0900

    .env 파일 장착

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'pages/home.dart';
 import 'pages/search.dart';
 import 'pages/login.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
