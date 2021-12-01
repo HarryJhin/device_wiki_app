b26883e1550bb030ffc790bc4cdbf7ed02fcde75
JJH <jujinhyen@gmail.com>
2021-12-01 10:01:47 +0900

    수정

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '장치위키',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
