305231443e950b759e175bda3c84896d1ffe07c3
JJH <jujinhyen@gmail.com>
2021-12-01 09:50:09 +0900

    수정

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
